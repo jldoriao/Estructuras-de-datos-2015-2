26d2c336fc4393e18450c594feb45d217a37f9df
Camilink94 <camilo.chaparro.co@gmail.com>
2015-09-14 01:17:58 -0500

    Adds creating figures from random data

--- Taller 1/Figures/src/Main.java
+++ Taller 1/Figures/src/Main.java
@@ -1,28 +1,46 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
 
-		System.out.println("Creating RectangleDs:");
-		System.out.println("\nRD1:");
-		RectangleD rd1 = new RectangleD(5, 2);
-		System.out.println("\nRD2:");
-		RectangleD rd2 = new RectangleD(5, 3);
-		System.out.println("\nRD3:");
-		RectangleD rd3 = new RectangleD(35, 25);
-		System.out.println("\nRD4:");
-		RectangleD rd4 = new RectangleD(7, 2);
-		System.out.println("\nRD5:");
-		RectangleD rd5 = new RectangleD();
+		float w;
+		float h;
+		float b;
+		int min = -2;
+		int max = 22;
+		float r;
+		Random rand = new Random();
+
 		RectangleD rdt;
-
 		ArrayList<RectangleD> recDList = new ArrayList<RectangleD>();
-		recDList.add(rd1);
-		recDList.add(rd2);
-		recDList.add(rd3);
-		recDList.add(rd4);
-		recDList.add(rd5);
 
+		Rectangle rt;
+		ArrayList<Rectangle> recList = new ArrayList<Rectangle>();
+
+		Triangle tt;
+		ArrayList<Triangle> trList = new ArrayList<Triangle>();
+
+		Circle ct;
+		ArrayList<Circle> cirList = new ArrayList<Circle>();
+
+		//************************RECTANGLED***************************
+		System.out.println("Creating RectangleDs:");
+		System.out.println("\nRD1 (default):");
+		rdt = new RectangleD();
+		recDList.add(rdt);
+
+		for (int i = 0; i < 4; i++) {
+			w = rand.nextFloat() * (max - min) + min;
+			h = rand.nextFloat() * (max - min) + min;
+			System.out.println("\nRD" + (i + 2) + ", w=" + w + ", h=" + h + ":");
+			rdt = new RectangleD(w, h);
+			recDList.add(rdt);
+		}
+		System.out.println();
+		
+		System.out.println("**************************");
+		System.out.println("\nRectangleDs' data:");
 		for (int i = 0; i < recDList.size(); i++) {
 			rdt = recDList.get(i);
 			System.out.println("\nRectangleD number " + (i + 1) + ":");
@@ -33,26 +51,23 @@ public class Main {
 		}
 		System.out.println();
 
+		//************************RECTANGLE****************************		
+		System.out.println("**************************");
 		System.out.println("\nCreating Rectangles:");
-		System.out.println("\nR1:");
-		Rectangle r1 = new Rectangle();
-		System.out.println("\nR2:");
-		Rectangle r2 = new Rectangle(5, 2);
-		System.out.println("\nR3:");
-		Rectangle r3 = new Rectangle(5, 3);
-		System.out.println("\nR4:");
-		Rectangle r4 = new Rectangle(35, 52);
-		System.out.println("\nR5:");
-		Rectangle r5 = new Rectangle(15, 23);
-		Rectangle rt;
-
-		ArrayList<Rectangle> recList = new ArrayList<Rectangle>();
-		recList.add(r1);
-		recList.add(r2);
-		recList.add(r3);
-		recList.add(r4);
-		recList.add(r5);
-
+		System.out.println("\nR1 (default):");
+		rt = new Rectangle();
+		recDList.add(rdt);
+		for (int i = 0; i < 4; i++) {
+			w = rand.nextFloat() * (max - min) + min;
+			h = rand.nextFloat() * (max - min) + min;
+			System.out.println("\nR" + (i + 2) + ", w=" + w + ", h=" + h + ":");
+			rt = new Rectangle(w, h);
+			recList.add(rt);
+		}
+		System.out.println();
+		
+		System.out.println("**************************");
+		System.out.println("\nRectangles' data:");
 		for (int i = 0; i < recList.size(); i++) {
 			rt = recList.get(i);
 			System.out.println("\nRectangle number " + (i + 1) + ":");
@@ -62,27 +77,24 @@ public class Main {
 			System.out.println("\tArea: " + rt.getArea());
 		}
 		System.out.println();
-
-		System.out.println("Creating Triangles:");
-		System.out.println("\nT1:");
-		Triangle t1 = new Triangle();
-		System.out.println("\nT2:");
-		Triangle t2 = new Triangle(2, 5);
-		System.out.println("\nT3:");
-		Triangle t3 = new Triangle(3, 3, 3);
-		System.out.println("\nT4:");
-		Triangle t4 = new Triangle(25, 15);
-		System.out.println("\nT5:");
-		Triangle t5 = new Triangle(2, 3, 8);
-		Triangle tt;
-
-		ArrayList<Triangle> trList = new ArrayList<Triangle>();
-		trList.add(t1);
-		trList.add(t2);
-		trList.add(t3);
-		trList.add(t4);
-		trList.add(t5);
-
+		
+		//************************TRIANGLE*****************************
+		System.out.println("**************************");
+		System.out.println("\nCreating Triangles:");
+		System.out.println("\nT1 (default):");
+		tt = new Triangle();
+		trList.add(tt);
+		for (int i = 0; i < 4; i++) {
+			b = rand.nextFloat() * (max - min) + min;
+			h = rand.nextFloat() * (max - min) + min;
+			System.out.println("\nT" + (i + 2) + ", b=" + b + ", h=" + h + ":");
+			tt = new Triangle(b, h);
+			trList.add(tt);
+		}
+		System.out.println();
+		
+		System.out.println("**************************");
+		System.out.println("\nTriangles' data:");
 		for (int i = 0; i < trList.size(); i++) {
 			tt = trList.get(i);
 			System.out.println("\nTriangle number " + (i + 1) + ":");
@@ -92,27 +104,29 @@ public class Main {
 			System.out.println("\tArea: " + tt.getArea());
 		}
 		System.out.println();
+		
+		//************************CIRCLE*******************************
+		System.out.println("**************************");
+		System.out.println("\nCreating Circles:");
+		System.out.println("\nC1 (default):");
+		ct = new Circle();
+		recDList.add(rdt);
+		for (int i = 0; i < 4; i++) {
+			r = rand.nextFloat() * (max - min) + min;
+			System.out.println("\nC" + (i + 2) + ", r=" + r + ":");
+			ct = new Circle(r);
+			cirList.add(ct);
+		}
+		System.out.println();
 
-		System.out.println("Creating Circles:");
-		System.out.println("\nC1:");
-		Circle c1 = new Circle();
-		System.out.println("\nC2:");
-		Circle c2 = new Circle(1);
-		System.out.println("\nC3:");
-		Circle c3 = new Circle(20);
-		System.out.println("\nC4:");
-		Circle c4 = new Circle(42);
-		System.out.println("\nC5:");
-		Circle c5 = new Circle(-1);
-		Circle ct;
+		
+		
+		
 
-		ArrayList<Circle> cirList = new ArrayList<Circle>();
-		cirList.add(c1);
-		cirList.add(c2);
-		cirList.add(c3);
-		cirList.add(c4);
-		cirList.add(c5);
+		
 
+		System.out.println("**************************");
+		System.out.println("\nCircles' data:");
 		for (int i = 0; i < cirList.size(); i++) {
 			ct = cirList.get(i);
 			System.out.println("\nCircle number " + (i + 1) + ":");
